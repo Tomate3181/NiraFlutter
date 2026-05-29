0905de4f093b659f23007ef7ac2fce069e492df5
Samuel Mioni <samumioni02@gmail.com>
2026-05-29 01:39:48 -0300

    tentando arrumar os erros da pagina conteudos 2

--- lib/screens/conteudos_screen.dart
+++ lib/screens/conteudos_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+// import 'package:riverpod/riverpod.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 import '../widgets/nira_glass_card.dart';
 import '../services/nira_supabase_service.dart';
